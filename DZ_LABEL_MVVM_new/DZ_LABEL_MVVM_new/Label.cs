6b41caf58c4dc26601c87eb31243bae2969e2301
Adelya <54155373+Delyaday@users.noreply.github.com>
2021-01-31 19:34:38 +0200

    Add files via upload

---
+++ DZ_LABEL_MVVM_new/DZ_LABEL_MVVM_new/Label.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace DZ_LABEL_MVVM
+{
+    public class Label : INotifyPropertyChanged
+    {
+        private Color background;
+        private double fontsize;
+        private string fontfamily;
+        private double height;
+        private double width;
+
+
+
+        public Color Background
+        {
+            get { return background; }
+            set
+            {
+                background = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Fontsize
+        {
+            get { return fontsize; }
+            set
+            {
+                fontsize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Fontfamily
+        {
+            get { return fontfamily; }
+            set
+            {
+                fontfamily = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                width = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ColorName
+        {
+            get
+            {
+                var colorName = typeof(Colors)
+                    .GetProperties()
+                    .FirstOrDefault(f => f.GetValue(null).ToString() == background.ToString());
+
+                return colorName != null ? colorName.Name : background.ToString();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DZ_LABEL_MVVM_new/DZ_LABEL_MVVM_new/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace DZ_LABEL_MVVM
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Label selectedLabel;
+        public ObservableCollection<Label> Labels { get; set; }
+
+        public Label SelectedLabel
+        {
+            get { return selectedLabel; }
+            set
+            {
+                selectedLabel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModel()
+        {
+            Labels = new ObservableCollection<Label>
+            {
+               new Label {Background = Colors.Violet, Fontsize = 22, Fontfamily="Algerian", Height = 150, Width = 250 },
+               new Label {Background = Colors.Pink, Fontsize = 20, Height = 175, Fontfamily="Tahoma", Width = 305},
+               new Label {Background = Colors.Green, Fontsize = 13, Height = 130, Fontfamily = "Segoe Print", Width = 190},
+               new Label {Background = Colors.Red, Fontsize = 15, Fontfamily = "Comic Sans Ms", Height = 115, Width = 185},
+            };
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
